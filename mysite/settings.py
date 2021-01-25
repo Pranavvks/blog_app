74d65e52cb2bef95adc2d4123db1fc130c78f663
Pranavvks <pranav.vks@gmail.com>
2021-01-25 18:43:08 +0530

    Added HTML for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -55,7 +56,8 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['blog/blog/templates/blog'],
+        #os.path.join(BASE_DIR,'blog','templates','blog')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
